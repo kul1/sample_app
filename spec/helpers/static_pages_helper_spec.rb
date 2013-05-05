49468fce3c4456f8471a1ae5849c825a9618ba75
Tim Kul <1.0@kul.com>
2013-05-05 16:34:07 +0700

    Finish static pages with error rspec yet

--- spec/helpers/static_pages_helper_spec.rb
+++ spec/helpers/static_pages_helper_spec.rb
@@ -1,15 +1,20 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the StaticPagesHelper. For example:
-#
-# describe StaticPagesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
-describe StaticPagesHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
 end
